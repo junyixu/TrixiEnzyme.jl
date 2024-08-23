b54b9998d898c52edf85c4af2188e41ab1e69187
Junyi Xu <junyixu0@gmail.com>
2024-08-23 23:38:18 +0800

    test

--- docs/src/anotherPage.md
+++ docs/src/anotherPage.md
@@ -1,1 +1,18 @@
 # The TrixiEnzyme Module
+
+```@docs
+TrixiEnzyme
+```
+
+## Module Index
+
+```@index
+Modules = [TrixiEnzyme]
+Order   = [:constant, :type, :function, :macro]
+```
+## Detailed API
+
+```@autodocs
+Modules = [TrixiEnzyme]
+Order   = [:constant, :type, :function, :macro]
+```
--- src/TrixiEnzyme.jl
+++ src/TrixiEnzyme.jl
@@ -1,4 +1,11 @@
+"""
+    TrixiEnzyme
+
+TrixiEnzyme.jl is a component package of the Trixi.jl ecosystem and integrates Trixi.jl with Compiler-Based (LLVM level) automatic differentiation via Enzyme.jl for hyperbolic partial differential equations (PDEs).
+The integration of Trixi.jl with Compiler-Based (LLVM level) automatic differentiation via Enzyme.jl offers the following benefits: facilitates rapid forward mode AD, enables reverse mode AD, supports cross-language AD, and critically, supports mutating operations and caching, on which Trixi.jl relies, to enhance the performance of both simulation runs and AD. The final deliverable will include as many of Trixi's advanced features as possible, such as adaptive mesh refinement, shock capturing, etc., showcasing the benefits of differentiable programming in Julia's ecosystem.
+"""
 module TrixiEnzyme
+
 export plusTwo
 
 greet() = print("Hello World!")
